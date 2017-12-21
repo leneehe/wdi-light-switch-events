1d6682d2366c05e9f0fcfc179d27465304196220
Lena <fatmeow@gmail.com>
2017-12-21 15:04:09 -0500

    Add evenListener to switch to toggle off/on

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  var button = document.querySelector('button.switch');
 
+  button.addEventListener('click', function(e) {
+    if (e.target.classList.contains('on')) {
+      e.target.classList.remove('on');
+      e.target.classList.add('off');
+    } else {
+      e.target.classList.remove('off');
+      e.target.classList.add('on');
+    }
+
+  })
+
+});
