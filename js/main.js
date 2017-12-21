0f0b26a46c7c0af852d42b7df2d90bc4c6ac8f7f
Lena <fatmeow@gmail.com>
2017-12-21 15:16:27 -0500

    Swap background colors to light/dark and status text

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var button = document.querySelector('button.switch');
+  var button = document.querySelector('button.switch'),
+      background = document.querySelector('body'),
+      status = document.querySelector('h1.status');
 
   button.addEventListener('click', function(e) {
     if (e.target.classList.contains('on')) {
       e.target.classList.remove('on');
       e.target.classList.add('off');
+      background.className = 'dark';
+      status.innerText = 'Hey, who turned off the lights?';
     } else {
       e.target.classList.remove('off');
       e.target.classList.add('on');
+      background.className = 'light';
+      status.innerText = "It's so bright in here!";
     }
 
   })
